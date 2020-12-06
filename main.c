c12003e3b66eab9c8962267896a1dccd5941cee2
danielberco <59802615+danielberco@users.noreply.github.com>
2020-12-06 19:56:29 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,72 @@
+#include "myBank.h"
+#include <stdio.h>
+
+int main() {
+    char action;
+    int acNum;
+    double amount =0, deposit =0, withdraw =0 , rate=0;
+
+    while (action != 'E') {
+        printf("Hello and welcome to the bank, please choose an action :\n");
+        printf(" O - Open new account\n B- Check account balance\n D - Make a deposit\n W - Make a withdrawal\n C - Close the account\n I - Add an interest\n P - print information\n E - Exit the bank\n");
+
+
+        if(scanf(" %c", &action) != 1) {
+            printf("Error has been made, please try again\n");
+        }
+
+        else {
+            switch (action) {
+                case 'O':
+                    printf("\n Please insert your amount in the acount :");
+                    scanf("%lf",&amount);
+                    openAcount(amount);
+                    break;
+                case 'B' :
+                    printf("\nEnter your account number:\n");
+                    balance(acNum);
+                    break;
+
+                case 'D' :
+                    printf("\nEnter your account number:\n");
+                    scanf("%d", &acNum);
+                    printf("\nEnter the amount would you like to deposit\n ");
+                    scanf("%lf",&deposit);
+                    depositB(acNum,deposit);
+                    break;
+
+                case 'W' :
+                    printf("\nEnter your account number:\n");
+                    scanf("%d", &acNum);
+                    printf("\nEnter the amount would you like to withdraw\n ");
+                    scanf("%lf",&withdraw);
+                    withdrawB(acNum,withdraw);
+                    break;
+
+                case 'C' :
+                    printf("\nEnter your account number:\n");
+                    scanf("%d", &acNum);
+                    closeAc(acNum);
+                    break;
+
+                case 'I' :
+                    printf("\nEnter your rate of the interest:\n");
+                    scanf("%le", &rate);
+                    addInterest(rate);
+                    break;
+
+                case 'P':
+                    printAc();
+                    break;
+
+                case 'E' :
+                    exitAll();
+                    break;
+
+                default:
+                    printf("Error has been made , please try again\n");
+                    break;
+            }
+        }
+    }
+}
